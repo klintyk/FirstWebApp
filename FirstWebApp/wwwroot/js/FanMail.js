3caf28282c7e42e26d0e194c81273da9ec314239
kyleklintworth <kyleklintworth@KYLEKLINTWO5956>
2023-01-30 17:01:38 -0700

    Add project files.

---
+++ FirstWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,9 @@
+﻿//document.getElementById("btn_send").addEventListener("click", function () {
+//    alert("From: " + document.getElementById("txt_from").value + ", Subject: " + document.getElementById("txt_subject").value + ", Message: " + document.getElementById("txt_msg"));
+//})
+
+$("#btn_send").click(function () {
+    alert("From: " + $("#txt_from").val() + ", Subject " + $("#txt_subject").val() + ", Message: " + $("#txt_msg").val());
+
+    $("#picgand").fadeToggle("slow");
+})
